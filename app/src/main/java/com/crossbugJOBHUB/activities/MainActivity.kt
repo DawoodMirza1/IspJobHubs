244f531b34dec7f9156a49cef989951d908ca4f6
Arbaz Mateen <arbazmateen@gmail.com>
2019-08-24 21:36:11 +0500

    setting up project resources

--- app/src/main/java/com/crossbugJOBHUB/activities/MainActivity.kt
+++ app/src/main/java/com/crossbugJOBHUB/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.crossbugJOBHUB
+package com.crossbugJOBHUB.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.crossbugJOBHUB.R
 
 class MainActivity : AppCompatActivity() {
 
